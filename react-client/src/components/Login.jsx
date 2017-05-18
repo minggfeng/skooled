a14767ae345cd1c1e81e95fdc7c22b487c2b042d
Salih <salih7@gmail.com>
2017-05-18 16:04:04 -0700

    Get rid of Login component flicker on refreshes from the dashboard

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -8,13 +8,26 @@ class Login extends React.Component {
 		super (props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      loading: true
     };
 
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
+  componentWillMount() {
+    if (this.props.isLoggedIn) {
+      this.props.history.push('/');
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false}); 
+    }, 500);
+  }
 
   handleUsernameChange(event) {
     this.setState({username: event.target.value});
@@ -24,43 +37,43 @@ class Login extends React.Component {
     this.setState({password: event.target.value});
   }
 
-  componentWillMount() {
-    if (this.props.isLoggedIn) {
-      this.props.history.push('/');
-    }
-  }
-
   handleSubmit() {
     this.props.enterCredentials(this.state.username, this.state.password);
   }
 
   render() {
-    return (
-      <div className="main container-fluid col-md-12" id="loginpage">
-        <div id="loginform">
-        <div id="logintitle">
-          <h2 > Welcome to Skooled! </h2>
-          <h3 > Log in: </h3>
+    if(this.state.loading) {
+      return (
+        null
+      );
+    } else {
+      return (
+        <div className="main container-fluid col-md-12" id="loginpage">
+          <div id="loginform">
+          <div id="logintitle">
+            <h2 > Welcome to Skooled! </h2>
+            <h3 > Log in: </h3>
+          </div>
+            <TextField
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleUsernameChange}
+              id="username"
+            />
+            <br></br>
+            <TextField
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              type="password"
+              id="password"
+            />
+            <br></br>
+            <RaisedButton label="Submit" primary={true} style={style} onClick={this.handleSubmit} />
+          </div>
         </div>
-          <TextField
-            placeholder="Username"
-            value={this.state.username}
-            onChange={this.handleUsernameChange}
-            id="username"
-          />
-          <br></br>
-          <TextField
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handlePasswordChange}
-            type="password"
-            id="password"
-          />
-          <br></br>
-          <RaisedButton label="Submit" primary={true} style={style} onClick={this.handleSubmit} />
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
