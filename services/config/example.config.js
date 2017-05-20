97ddb3930e85bcfab356b1c2148a56b3122b82e2
Salih <salih7@gmail.com>
2017-05-19 19:35:39 -0700

    Adjust naming of Twilio keys for better identification

--- services/config/example.config.js
+++ services/config/example.config.js
@@ -7,6 +7,6 @@ module.exports = {
   PASSPORT_JWT_SECRETORKEY : '',
   MAILGUN_API_KEY : '',
   MAILGUN_DOMAIN : '',
-  ACCOUNT_SID : '',
-  AUTH_TOKEN : ''
+  TWILIO_ACCOUNT_SID : '',
+  TWILIO_AUTH_TOKEN : ''
 };
